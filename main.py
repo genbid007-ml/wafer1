91d527729cdf930c9995e2ef5518d0a9b1fc85de
genbid007-ml <genbid007.ml@gmail.com>
2021-11-20 08:23:44 -0500

    second commit

--- main.py
+++ main.py
@@ -15,15 +15,14 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    return "Flask app is running"
+    return "Flask app is running by Bidyut"
 
-#port = int(os.getenv("PORT", 9001))
+port = int(os.getenv("PORT", 5001))
 
 if __name__ == "__main__":
-    host = '127.0.0.1'
-    port =9001
+    host = '0.0.0.0'
     #app.run()
-    #httpd = simple_server.make_server(host=host, port=port, app=app)
-    httpd = simple_server.make_server(host='127.0.0.1', port=9001, app=app)
-    print("Serving on %s %d" % (host, port))
+    httpd = simple_server.make_server(host=host, port=port, app=app)
+    #httpd = simple_server.make_server(host='127.0.0.1', port=5000, app=app)
+    # print("Serving on %s %d" % (host, port))
     httpd.serve_forever()
